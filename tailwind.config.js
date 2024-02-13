fcf488b4f35940efcab428b2d04151165a9953cc
6nsfour <diaryrakoto@gmail.com>
2024-02-13 10:06:33 +0100

    feat: setup theme for daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        rcr: {
+          "primary": "#284B63",
+          "secondary": "#B4B8AB",
+          "accent": "#153243",
+          "neutral": "#F4F9E9",
+          "base-100": "#EEF0EB",
+          "info": "#bfdbfe",
+          "success": "#4ade80",
+          "warning": "#b32e00",
+          "error": "#ff3656",
+        },
+      },
+    ],
+  },
   content: [
     "./src/**/*.{html,ts}",
   ],
